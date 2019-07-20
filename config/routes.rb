bbcf91d9e95471fff31e9f34dd70312a64142182
Daisaku Sawada <d_sawada@hotmail.co.jp>
2019-07-20 09:46:34 +0000

    Finish page-layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-root 'static_pages#top'
+  root 'static_pages#top' # static_page/top.html.erbをルートページとして設定
+  get '/signup', to: 'users#new'
+
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
