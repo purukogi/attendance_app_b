cacc02aede7325fdd95f7dd1e41c4df0ac96086a
Daisaku Sawada <d_sawada@hotmail.co.jp>
2019-08-11 11:27:40 +0000

    2019/08/11_save2

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -3,12 +3,22 @@ module AttendancesHelper
   def attendance_state(attendance)
     # 受け取ったAttendanceオブジェクトが当日と一致するか評価します。
     if Date.current == attendance.worked_on
-      return '出社' if attendance.started_at.nil?
+      return '出社' if attendance.started_at.nil? # 出勤時間が空欄の場合、出社ボタンを表示
+    end
+    # どれにも当てはまらなかった場合はfalseを返します。
+    false
+  end
+  
+  def attendance_state2(attendance)
+    # 受け取ったAttendanceオブジェクトが当日と一致するか評価します。
+    if Date.current == attendance.worked_on
       return '退社' if attendance.started_at.present? && attendance.finished_at.nil?
     end
     # どれにも当てはまらなかった場合はfalseを返します。
     false
   end
+  
+  
 
   # 出勤時間と退勤時間を受け取り、在社時間を計算して返します。
   def working_times(start, finish)
